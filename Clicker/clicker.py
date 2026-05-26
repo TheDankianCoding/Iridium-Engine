5dec65ee7e4883069997704faf7d4f2031e9c161
TheDankianCoding <DankianGamer@Gmail.com>
2026-05-25 19:39:09 -0600

    Add files via upload

---
+++ Clicker/clicker.py
@@ -0,0 +1,12 @@
+import pygame
+pygame.init()
+
+
+#Variables
+pydis = pygame.display
+scrn = pydis.set_mode((300, 400))
+
+
+#Screen
+pydis.set_caption("Not Adventure Capitalist - Credit To LeMaster Tech For The Tutorial")
+
